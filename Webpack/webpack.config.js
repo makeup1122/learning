92afe506539316f6e8707b0c8b2c75bda0babb24
libing <makeup1123@163.com>
2017-09-25 17:26:02 +0800

    webpack-管理资源

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -5,5 +5,30 @@ module.exports = {
     output:{
         filename:'bundle.js',
         path:path.resolve(__dirname,'dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            },
+            {
+                test:/\.less$/,
+                use:['style-loader','css-loader','less-loader']
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:['file-loader']
+            },
+            {
+                test: /\.(html)$/,
+                use: {
+                  loader: 'html-loader',
+                  options: {
+                    attrs: [':data-src']
+                  }
+                }
+            }
+        ]
     }
 }
