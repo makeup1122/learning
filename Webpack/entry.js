4fee4081246a99ef2f2eeec2cc1d746085f7ddd8
libing <makeup1123@163.com>
2017-09-25 16:04:27 +0800

    clear webpack dir

--- Webpack/entry.js
+++
@@ -1,6 +0,0 @@
-// require("!style-loader!css-loader!./style.css"); // 载入 style.css
-require('./style.css');
-//ro 
-//require("./style.css")  with  webpack entry.js bundle.js --module-bind 'css=style!css'
-document.write('It works.');
-document.write(require('./module.js'));//添加模块
