06f9a0a218e2bc3fbda6012acb5e9ce6cf6c0925
libing <makeup1123@163.com>
2017-09-26 10:55:12 +0800

    HMR (hot modelu replacement)

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,10 +1,23 @@
 const path= require('path');
-
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     entry:'./src/index.js',
     output:{
         filename:'bundle.js',
-        path:path.resolve(__dirname,'dist')
+        path:path.resolve(__dirname,'dist'),
+        publicPath: '/dist/',
+    },
+    plugins:[
+        new CleanWebpackPlugin(['dist']),
+        new webpack.HotModuleReplacementPlugin(),
+        new UglifyJSPlugin() //tree shaking  精简输出，删除未引用代码
+    ],
+    devServer:{
+        compress: true,
+        noInfo: true,   //只显示错误和警告
+        hot:true,       //启用模块热替换
+        // contentBase:'./dist'
     },
     module:{
         rules:[
